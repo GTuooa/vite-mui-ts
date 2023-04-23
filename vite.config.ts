ba3620f766f9281575ef427f6d0a95bf7059f401
GTuooa <1659256785@qq.com>
2023-04-23 22:59:23 +0800

    添加兼容性配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import * as path from 'path';
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import * as path from 'path'
+import legacy from '@vitejs/plugin-legacy'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {},
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+  const plugins = [react()]
+  if (isBuild) {
+    plugins.push(legacy())
+  }
+  return {
+    plugins,
+    build: {},
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-});
+  }
+})
